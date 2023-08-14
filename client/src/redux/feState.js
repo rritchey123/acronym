8756491c350d88aaadd8cebfda86c15d01073864
Ryan Ritchey <ryan.ritchey123@gmail.com>
2023-08-13 19:48:56 -0700

    Stub functionality to keep track of roomId on front end

--- client/src/redux/feState.js
+++ client/src/redux/feState.js
@@ -3,14 +3,18 @@ import { createSlice } from "@reduxjs/toolkit"
 export const feStateSlice = createSlice({
     name: "feState",
     initialState: {
-        feState: "home"
+        feState: "home",
+        roomId: ""
     },
     reducers: {
         setFeState: (state, action) => {
             state.feState = action.payload
+        },
+        setRoomId: (state, action) => {
+            state.roomId = action.payload
         }
     }
 })
 
-export const { setFeState } = feStateSlice.actions
+export const { setFeState, setRoomId } = feStateSlice.actions
 export default feStateSlice.reducer
