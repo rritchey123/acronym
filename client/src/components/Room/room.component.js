5545ee37ca27e3fe3615cd4e9e0b850867d38d4f
Ryan Ritchey <ryan.ritchey123@gmail.com>
2023-10-17 22:04:36 -0700

    Render player type in UI and optional start game button

--- client/src/components/Room/room.component.js
+++ client/src/components/Room/room.component.js
@@ -8,11 +8,15 @@ import { useSelector } from 'react-redux';
 import socket from '../../socket';
 
 export function Room() {
-    const { roomId } = useSelector((state) => state.feState)
+    const { roomId, playerType } = useSelector((state) => state.feState)
     const { connected } = useSelector((state) => state.connectionState)
 
     function leaveRoom() {
         socket.emit("leave-room", { roomId })
+    }
+
+    function startGame() {
+        socket.emit("start-game", { roomId })
     }
 
     return (
@@ -20,6 +24,10 @@ export function Room() {
             <h1>ROOM SCREEN</h1>
             <h1>roomId: {roomId || "No room"}</h1>
             <h1>connected : {connected ? "We connected" : "Negatory on that connection son"}</h1>
+            <h1>playerType : {playerType} </h1>
+            {
+                playerType === "leader" && <button type="button" onClick={startGame}>Start Game</button>
+            }
             <button type="button" onClick={leaveRoom} >Leave Room</button>
         </div>
     );
