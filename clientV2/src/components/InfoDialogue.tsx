fbec38ace48959dc424ed68e7fc54aaff76e596c
Ryan Ritchey <ryan.ritchey123@gmail.com>
2025-07-06 17:25:14 -0700

    Update info dialog content

--- clientV2/src/components/InfoDialogue.tsx
+++ clientV2/src/components/InfoDialogue.tsx
@@ -8,13 +8,14 @@ import {
     DialogTitle,
     DialogTrigger,
 } from '@/components/ui/dialog'
+import { AlarmClockPlus, MessageSquarePlus } from 'lucide-react'
 
 export function InfoDialogue() {
     return (
         <>
             <Dialog>
                 <DialogTrigger asChild>
-                    <Button className="mb-4 w-40">Info</Button>
+                    <Button className={'mb-4 w-40'}>Info</Button>
                 </DialogTrigger>
 
                 <DialogContent className="sm:max-w-[425px] space-y-6">
@@ -30,23 +31,26 @@ export function InfoDialogue() {
                     <div className="space-y-3 text-sm text-foreground leading-relaxed">
                         <p>
                             Each round, you’ll receive a <strong>prompt</strong>{' '}
-                            and an <strong>acronym</strong>.
-                        </p>
-                        <p>
-                            Your job is to submit what you think the acronym
-                            stands for, based on the prompt.
+                            and an <strong>acronym</strong>. Your job is to
+                            submit what you think the <strong>acronym</strong>{' '}
+                            stands for, based on the <strong>prompt</strong>.
                         </p>
                         <p>
                             Once all submissions are in, vote for the answer you
-                            think is best.
+                            think is best. You earn <strong>1 point</strong> for
+                            every vote your answer receives. The first player to
+                            reach <strong>10 points</strong> wins the game!
                         </p>
                         <p>
-                            You earn <strong>1 point</strong> for every vote
-                            your answer receives.
-                        </p>
-                        <p>
-                            The first player to reach <strong>10 points</strong>{' '}
-                            wins the game!
+                            Suggest prompts and acronyms using the{' '}
+                            <p className="inline-block">
+                                <MessageSquarePlus />
+                            </p>{' '}
+                            button. Add more time to the round using the{' '}
+                            <p className="inline-block">
+                                <AlarmClockPlus />
+                            </p>{' '}
+                            button.
                         </p>
                     </div>
 
